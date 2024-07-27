2368364831fe4f307e0901a5f7fd4e25b2101e5f
Sajal0208 <sajaldewangan@gmail.com>
2024-07-27 16:06:05 +0530

    addedd mail button

---
+++ src/components/mail-button.tsx
@@ -0,0 +1,7 @@
+export const Mailto = ({ email, subject = '', body = '', children }: { email: string, subject?: string, body?: string, children: React.ReactNode }) => {
+  let params = subject || body ? '?' : '';
+  if (subject) params += `subject=${encodeURIComponent(subject)}`;
+  if (body) params += `${subject ? '&' : ''}body=${encodeURIComponent(body)}`;
+
+  return <a href={`mailto:${email}${params}`}>{children}</a>;
+};
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { FaGithub, } from "react-icons/fa";
-import { FaXTwitter, FaLinkedinIn } from "react-icons/fa6";
+import { FaXTwitter, FaLinkedinIn, FaEnvelope } from "react-icons/fa6";
 import { GoMoveToTop } from "react-icons/go";
 import { useEffect, useState } from "react";
 import { FiGithub } from "react-icons/fi";
@@ -15,6 +15,11 @@ interface Link {
 }
 
 const links: Link[] = [
+  {
+    id: "Mail",
+    href: "mailto:sajaldewangan@gmail.com",
+    icon: FaEnvelope
+  },
   {
     id: "X",
     href: "https://x.com/DewanganSajal",
@@ -39,7 +44,7 @@ const links: Link[] = [
 
 export default function Sidebar() {
   return <div className='hidden md:flex flex-col fixed top-0 left-0 w-[80px] h-full bg-transparent justify-center items-center'>
-    <div className="flex flex-col gap-y-8 flex-1 justify-end">
+    <div className="flex flex-col gap-y-8 flex-1 mb-10 justify-end">
       {links.map((link: Link) => (
         <Link target="_blank" key={link.id} href={link.href}>
           <link.icon className="text-3xl text-gray-300 transition-all duration-500 ease-in-out hover:text-gray-800 hover:scale-110" />
