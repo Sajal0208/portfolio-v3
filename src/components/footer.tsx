8fc5b82741bb9ca64267ac8bf25414adf9798619
Sajal0208 <sajaldewangan@gmail.com>
2024-07-27 16:05:50 +0530

    Added footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,5 @@
+export default function Footer() {
+  return <div className="fixed bottom-5 tracking-widest w-full text-gray-300 text-sm">
+    © 2024 Design and build by Sajal Dewangan
+  </div>
+}
