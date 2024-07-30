6d9cd04c125b97d577943d0fd6164c2f3c17059d
Sajal0208 <sajaldewangan@gmail.com>
2024-07-30 13:31:24 +0530

    added onload animation

--- src/components/main-header.tsx
+++ src/components/main-header.tsx
@@ -8,7 +8,6 @@ const navLinks = [
   { label: "Home", href: "/" },
   { label: "Experience", href: "/experience" },
   { label: "Projects", href: "/projects" },
-  { label: "Projects", href: "/projects" },
   { label: "Contact", href: "/contact" },
 ]
 
@@ -16,9 +15,9 @@ export function MainHeader() {
   const pathname = usePathname();
 
   return (
-    <div className="flex gap-x-32 py-10 items-start">
+    <div className="flex justify-between sm:justify-start mb-10 sm:gap-x-32 py-10 items-start">
       <Logo />
-      <div className="grid grid-cols-2 gap-x-24">
+      <div className="grid grid-cols-2 gap-x-16 sm:gap-x-24">
         {navLinks.map((link) => (
           <NavLink key={link.href} label={link.label} href={link.href} isActive={pathname === link.href} />
         ))}
@@ -30,10 +29,10 @@ export function MainHeader() {
 const NavLink = ({ label, href, isActive }: { label: string; href: string; isActive: boolean }) => {
   return (
     <Link
-      className={`text-lg font-medium ${isActive
-        ? "text-gray-800 font-bold"
+      className={`text-xl font-medium ${isActive
+        ? "text-black font-bold"
         : "text-primary hover:text-gray-800"
-        } transition-all duration-300 ease-in-out hover:scale-105`}
+        } transition-all duration-300 ease-in-out hover:scale-105 uppercase`}
       href={href}
     >
       {label}
--- src/components/onload-animation.tsx
+++ src/components/onload-animation.tsx
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 
 export default function OnloadAnimation() {
   const text = "Welcome To Sajal Dewangan's Portfolio".split(" ");
-  return <div className="text-3xl md:text-9xl flex items-center justify-center h-screen">
+  return <div className="text-3xl text-primary md:text-9xl flex items-center justify-center h-screen">
     {text.map((el, i) => (
       <motion.span
         initial={{ opacity: 0 }}
