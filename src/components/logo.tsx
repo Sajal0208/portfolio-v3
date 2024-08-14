222103c56eef94daa535bbafd771e90a590e8b48
Sajal0208 <sajaldewangan@gmail.com>
2024-08-14 12:31:56 +0530

    Changed logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -6,5 +6,5 @@ import { useRouter } from "next/navigation";
 export function Logo() {
   const router = useRouter();
 
-  return <div className="text-5xl text-primary cursor-pointer" onClick={() => router.push("/")}><Image src="/SD.svg" alt="logo" width={200} height={200} /></div>;
+  return <div className="text-5xl text-primary cursor-pointer" onClick={() => router.push("/")}><Image src="SDSVG cropped.svg" alt="logo" width={200} height={200} /></div>;
 }
